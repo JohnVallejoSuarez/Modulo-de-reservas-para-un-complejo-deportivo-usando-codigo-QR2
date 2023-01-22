97652a4d9c03a1c7324b10fddc88ba8e3341e08d
Jhon Vallejo <jvallejos2@unemi.edu.ec>
2023-01-22 00:46:01 -0500

    Creacion de los modelos

--- reservas/admin.py
+++ reservas/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Disiplina, Instalacion, Reserva
 # Register your models here.
+
+class DisiplinaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'estado')
+
+class InstalacionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'descripcion', 'restriciones', 'precio', 'capacidad', 'imagen', 'indumentaria', 'recomendaciones', 'equipo_incluido', 'estado')
+
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombres', 'apellidos', 'cedula', 'email', 'telefono', 'fecha_reservacion', 'fecha_reservada', 'codigo_qr', 'pago', 'hora_inicio', 'hora_fin', 'estado')
+
+
+admin.site.register(Disiplina, DisiplinaAdmin)
+admin.site.register(Instalacion, InstalacionAdmin)
+admin.site.register(Reserva, ReservaAdmin)
--- reservas/models.py
+++ reservas/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from datetime import datetime #tiempo
 # Create your models here.
+
+class Disiplina(models.Model):
+    nombre = models.CharField(max_length=10, unique=True)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Instalacion(models.Model):
+    id_diciplina = models.ForeignKey(Disiplina, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=30, unique=True)
+    descripcion = models.CharField(max_length=200)
+    restriciones = models.CharField(max_length=200)
+    precio = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    capacidad = models.CharField(max_length=10)
+    imagen = models.ImageField(upload_to='imagenes/instalaciones/%Y/%m/%d/')
+    indumentaria = models.CharField(max_length=200)
+    recomendaciones = models.CharField(max_length=200)
+    equipo_incluido = models.CharField(max_length=200)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Reserva(models.Model):
+    id_diciplina = models.ForeignKey(Disiplina, on_delete=models.CASCADE)
+    nombres = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=30)
+    cedula = models.CharField(max_length=10)
+    email = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=10)
+    fecha_reservacion = models.DateField(default=datetime.now)
+    fecha_reservada = models.DateField()
+    codigo_qr = models.CharField(max_length=255, unique=True)
+    pago = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    hora_inicio = models.DateTimeField()
+    hora_fin =models.DateTimeField()
+    estado = models.BooleanField(default=True)
